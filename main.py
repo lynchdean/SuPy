8d5cb0057959af4f3f1e0677f21e5ba35bfcce7e
Dean <14879888+lynchdean@users.noreply.github.com>
2019-10-17 21:22:59 +0100

    add requirements.txt

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ def main():
 
     cat_tags = soup.find("ul", id="nav-categories")
 
-    for link in cat_tags[1:]:
-        print(link.contents[0].get('href'))
+    cat_url_tails = [link.contents[0].get('href') for link in cat_tags][2:]
+
+    for x in cat_url_tails:
+        print(x)
 
 if __name__ == '__main__':
     main()
