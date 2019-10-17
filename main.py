55e08e30dc74df10d1f00402e1fb37004defc04f
Dean <14879888+lynchdean@users.noreply.github.com>
2019-10-17 22:29:10 +0100

    progress

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from bs4 import BeautifulSoup
 
 def main():
     # driver = webdriver.Chrome('./chromedriver')
-    base_url = "https://www.supremenewyork.com/shop/all"
+    base_url = "https://www.supremenewyork.com"
 
-    page = requests.get(base_url)
+    page = requests.get(base_url + "/shop/all")
     soup = BeautifulSoup(page.content, features="lxml")
 
     cat_tags = soup.find("ul", id="nav-categories")
-
     cat_url_tails = [link.contents[0].get('href') for link in cat_tags][2:]
 
-    for x in cat_url_tails:
-        print(x)
+    for tail in cat_url_tails:
+        cat_page = requests.get(base_url + tail)
+        cat_soup = BeautifulSoup(cat_page.content, features="lxml")
+        inner_articles = cat_soup.findAll("div", class_="inner-article")
+        for x in inner_articles:
+            print(x)
+
+
 
 if __name__ == '__main__':
     main()
