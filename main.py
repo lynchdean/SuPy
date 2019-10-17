e143a62c59cc46be2d539f0e7b50d6df4aca6c80
Dean Lynch <14879888+lynchdean@users.noreply.github.com>
2019-10-17 18:43:57 +0100

    Minor progress & idea files

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 
 def main():
     # driver = webdriver.Chrome('./chromedriver')
-    base_url = "https://www.supremenewyork.com/shop/all"
+    base_url = "https://www.supremenewyork.com"
 
-    page = requests.get(base_url)
+    page = requests.get(base_url + "/shop/all")
     soup = BeautifulSoup(page.content, features="lxml")
 
+    # Get the url tails for each of the categories
     cat_tags = soup.find("ul", id="nav-categories")
+    cat_url_tails = [link.contents[0].get('href') for link in cat_tags][2:]
 
-    for link in cat_tags[1:]:
-        print(link.contents[0].get('href'))
 
 if __name__ == '__main__':
     main()
